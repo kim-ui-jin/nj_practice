6a4ad9e9548323b162ecfba6809f34e4aba452c3
Kim-ui-jin <yd5663@gmail.com>
2025-08-25 16:02:35 +0900

    controller 실습

--- nest-first/src/cats/cats.controller.ts
+++ nest-first/src/cats/cats.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateCatDto } from './CreateCatDto';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+
+    // 전체 목록을 조회하는 메서드
+    @Get()
+    findAll(): string {
+        return "This action returns all cats";
+    }
+
+    // 단건 상세조회
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `This action returns a #${id} cat`;
+    }
+
+    @Post()
+    create(@Body() createCatDto: CreateCatDto) {
+        return 'This action adds a new cat';
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() createCatDto: CreateCatDto) {
+        return `This action updates a #${id} cat`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} cat`;
+    }
+
+}
