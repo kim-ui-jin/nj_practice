26aecdbf8eba15e941b3b87fbc0845d7e531a0d9
Kim-ui-jin <yd5663@gmail.com>
2025-08-26 16:07:20 +0900

    회원가입 실습2

--- nest-typeorm/src/auth/auth.controller.ts
+++ nest-typeorm/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserDTO } from './dto/user.dto';
+import { User } from './entity/user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post('register')
+    registerAccount(@Body() userDto: UserDTO): Promise<User> {
+        return this.authService.registerUser(userDto);
+    }
+}
