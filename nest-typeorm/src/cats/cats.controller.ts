c2668ffd8fc8d88e329eb28180654407fe74ca50
Kim-ui-jin <yd5663@gmail.com>
2025-08-26 14:17:06 +0900

    typeorm 실습2

--- nest-typeorm/src/cats/cats.controller.ts
+++ nest-typeorm/src/cats/cats.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat } from './entity/cats.entity';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    constructor(private catsService: CatsService) { };
+
+    // 전체 조회
+    @Get()
+    findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
+    }
+
+    // 단건 조회
+    @Get(':id')
+    findOne(@Param('id') id: number): Promise<Cat | null> {
+        return this.catsService.findOne(id);
+    }
+
+    // cat 데이터 생성
+    @Post()
+    create(@Body() cat: Cat) {
+        this.catsService.create(cat);
+    }
+
+    // cat 데이터 삭제
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+        this.catsService.remove(id);
+    }
+
+    // 정보 수정
+    @Put(':id')
+    update(@Param('id') id: number, @Body() cat: Cat) {
+        this.catsService.update(id, cat);
+        return `This action updates a #${id} cat`;
+    }
+}
--- nest-typeorm/src/cats/cats.service.ts
+++ nest-typeorm/src/cats/cats.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Cat } from './entity/cats.entity';
-import { Repository } from 'typeorm';
+import { getConnection, Repository } from 'typeorm';
 
 @Injectable()
 export class CatsService {
@@ -24,5 +24,24 @@ export class CatsService {
 
     async remove(id: number): Promise<void> {
         await this.catsRepository.delete(id);
+    }
+
+    async update(id: number, cat: Cat): Promise<void> {
+        // 데이터가 있을 경우만 처리
+        const existedCat = await this.catsRepository
+            .createQueryBuilder()
+            .update(Cat)
+            .set({
+                name: cat.name,
+                age: cat.age,
+                breed: cat.breed
+            })
+            .where("id=:id", { id })
+            .execute();
+
+        if (existedCat.affected === 0) {
+            throw new NotFoundException(`Cat With id #${id} not found`);
+        }
+
     }
 }
