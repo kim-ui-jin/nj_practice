b49a4c81e7074c9591d28368e64fd9c6988f93f4
Kim-ui-jin <yd5663@gmail.com>
2025-08-27 18:17:34 +0900

    권한 관리2 실습

---
+++ nest-typeorm/src/auth/security/roles.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { User } from "../entity/user.entity";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+    constructor(private readonly reflector: Reflector) { }
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const roles = this.reflector.get<string[]>('roles', context.getHandler()); // context의 핸들러를 읽어올 수 있음
+
+        if (!roles) return true;
+
+        const request = context.switchToHttp().getRequest();
+        const user = request.user as User;
+
+        return !!user?.authorities?.some(role => roles.includes(role));
+    }
+}
+// CanActivate -> 사용 가능한지 여부를 묻는다
+// Reflector -> 프로그램 실행 시 메타데이터를 가져올 수 있게하는 구현체
