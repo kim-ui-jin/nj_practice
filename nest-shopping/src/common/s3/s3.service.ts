6a0cedcfa4ada7f73b881c599f3f022c89290f96
Kim-ui-jin <yd5663@gmail.com>
2025-12-16 16:38:50 +0900

    aws S3 파일 업로드 2

--- nest-shopping/src/common/s3/s3.service.ts
+++ nest-shopping/src/common/s3/s3.service.ts
@@ -14,10 +14,12 @@ export class S3Service {
 
     private readonly s3 = new S3Client({
         region: this.region,
-        credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
-        }
+        credentials: process.env.AWS_ACCESS_KEY_ID && process.env.AWS_SECRET_ACCESS_KEY
+            ? {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
+            }
+            : undefined
     });
 
     async uploadImage(
